b7b4d571c8cf75dbd5a9b72dd6a864b941c88253
RC_Student_lab <RC_Student_lab@10.0.0.151>
2024-10-30 09:03:26 +0000

    FirstProject

--- src/main/java/com/mycompany/firstproject/FirstProject.java
+++ src/main/java/com/mycompany/firstproject/FirstProject.java
@@ -5,7 +5,8 @@
 package com.mycompany.firstproject;
 
 import java.util.Scanner;
-
+import javax.lang.model.SourceVersion;
+import javax.swing.JOptionPane;
 /**
  *
  * @author RC_Student_lab
@@ -33,23 +34,66 @@ public class FirstProject {
         System.out.print("Enter password to login: ");
         String loginPassword = scanner.nextLine();
         login.returnLoginStatus(loginUsername, loginPassword);
-    
+        
+          System.out.println("Welcome to EasyKanban");
+
+        int numTasks = Integer.parseInt(JOptionPane.showInputDialog("Enter number of tasks:"));
+        System.out.println("Number of tasks: " + numTasks);
+
+        int totalHours = 0;
+
+        for (int i = 0; i < numTasks; i++) {
+            String taskName = JOptionPane.showInputDialog("Enter task name:");
+            System.out.println("Task name: " + taskName);
+
+            String taskDescription = JOptionPane.showInputDialog("Enter task description:");
+            while (taskDescription.length() > 50) {
+                System.out.println("Description too long. Please enter a description of less than 50 characters.");
+                taskDescription = JOptionPane.showInputDialog("Enter task description:");
+            }
+            System.out.println("Task description: " + taskDescription);
+
+            String developerDetails = JOptionPane.showInputDialog("Enter developer details:");
+            System.out.println("Developer details: " + developerDetails);
+
+            int taskDuration = Integer.parseInt(JOptionPane.showInputDialog("Enter task duration (hours):"));
+            System.out.println("Task duration: " + taskDuration + " hours");
+
+            String[] statuses = {"To Do", "Done", "Doing"};
+            String taskStatus = (String) JOptionPane.showInputDialog(null, "Select task status:", "Task Status", JOptionPane.QUESTION_MESSAGE, null, statuses, statuses[0]);
+            System.out.println("Task status: " + taskStatus);
+
+            Task task = new Task(taskName, i, taskDescription, developerDetails, taskDuration, taskStatus);
+            totalHours += task.totalHours();
+            System.out.println(task.printDetails());
+        }
+        System.out.println("Total hours: " + totalHours);
+    }
+       
     }
 
-    private static class Login {
+    class Login {
 
         public Login() {
         }
 
-        private void returnLoginStatus(String loginUsername, String loginPassword) {
+        void returnLoginStatus(String loginUsername, String loginPassword) {
             throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         }
 
-        private void registerUser(String username, String password, String firstName, String lastName) {
+        void registerUser(String username, String password, String firstName, String lastName) {
             throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         }
     }
 
-}
+   
+
+
+  
+
+
+     
+
+
 
    
